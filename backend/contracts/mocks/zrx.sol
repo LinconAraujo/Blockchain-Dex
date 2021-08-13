add3fa8cbf713299147a003e4d91da17368685b7
LinconAraujo <linconv.araujo@gmail.com>
2021-08-13 16:06:09 -0300

    Decentralized exchange

---
+++ backend/contracts/mocks/zrx.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol'; 
+
+contract Zrx is ERC20 {
+  constructor() ERC20('ZRX', '0x token') {}
+
+  function faucet(address to, uint amount) external {
+    _mint(to, amount);
+  }
+}
