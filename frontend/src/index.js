625660a69441603142111a8226eabba8751629bc
LinconAraujo <linconv.araujo@gmail.com>
2021-08-13 16:12:15 -0300

    Decentralized exchange

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/index.scss";
+
+import LoadingContainer from "./LoadingContainer.js";
+
+ReactDOM.render(<LoadingContainer />, document.getElementById("root"));
